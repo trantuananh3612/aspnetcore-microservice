8eba3a223d39bcf4591cd732bb27cf7df8101ddf
TruongVQ-WL <anhtt@reactplus.com>
2024-08-12 14:21:46 +0700

    - Buid Infrastructure with Docker Component - Build Logger Service with Serilog

--- src/BuildingBlocks/Common.Logging/SeriLogger.cs
+++ src/BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -1,7 +1,23 @@
-﻿namespace Common.Logging
-{
-    public class SeriLogger
-    {
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
 
+namespace Common.Logging
+{
+    public static class SeriLogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+        {
+            var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "_");
+            var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
+
+            configuration.WriteTo.Debug()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithProperty("Environment", environmentName)
+            .Enrich.WithProperty("Application", applicationName)
+            .ReadFrom.Configuration(context.Configuration);
+
+        };
     }
 }
